228d5c3f0558b3f09ca4df0e69addca376c26069
BorisArco <boris.arco@gmail.com>
2022-04-28 21:52:45 -0400

    add validaciones

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,16 @@
 class ProjectsController < ApplicationController
   def index
+    # @projects = Project.all.order('created_at DESC')
   end
 
   def projects
+   @project = Project.create(
+    name: params[:name],
+    description: params[:description],
+    created_at: params[:created_at],
+    finish_date: params[:finish_date],
+    state: params[:state])
+    @projects = Project.all.order('created_at DESC')
+
   end
 end
