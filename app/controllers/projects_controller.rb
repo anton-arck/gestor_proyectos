e86a66b38c4c72613bfd04c09d3cd43e4fca10e1
BorisArco <boris.arco@gmail.com>
2022-05-02 18:47:22 -0400

    Add status filter

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,16 +1,22 @@
 class ProjectsController < ApplicationController
   def index
-    # @projects = Project.all.order('created_at DESC')
   end
 
   def projects
-   @project = Project.create(
+    
+    @project = Project.create(
     name: params[:name],
     description: params[:description],
     created_at: params[:created_at],
     finish_date: params[:finish_date],
-    state: params[:state])
-    @projects = Project.all.order('created_at DESC')
-
+    state: params[:state]
+   )
+  
+    @filtrado=params[:filter]
+    if @filtrado == "empty"
+      @projects = Project.all.order('created_at DESC') 
+    else  
+      @projects = Project.all.order('created_at DESC').where(state: @filtrado) 
+    end
   end
 end
